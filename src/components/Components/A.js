86d438ca4b135e4e66bb0f1a240ce9a472d850f8
watcharapat <plengslowtoad>
2026-02-18 14:00:54 +0700

    Implement the a HTML tag (link) A.js

--- src/components/Components/A.js
+++ src/components/Components/A.js
@@ -1,1 +1,15 @@
-export const A = () => {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const A = ({ values }) => (
+  <a href={values.href} target="_blank" rel="noopener noreferrer">
+    {values.label}
+  </a>
+);
+
+A.propTypes = {
+  values: PropTypes.shape({
+    href: PropTypes.string,
+    label: PropTypes.string,
+  }).isRequired,
+};
