f098e6b0833abb9630ca9148851cbd266d7b1c66
watcharapat <plengslowtoad>
2026-02-18 14:03:30 +0700

    Implement the img HTML tag (image)

--- src/components/Components/Img.js
+++ src/components/Components/Img.js
@@ -1,1 +1,13 @@
-export const Img = () => {}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Img = ({ values }) => (
+  <img alt={values.alt} src={values.src} />
+);
+
+Img.propTypes = {
+  values: PropTypes.shape({
+    alt: PropTypes.string,
+    src: PropTypes.string,
+  }).isRequired,
+};
