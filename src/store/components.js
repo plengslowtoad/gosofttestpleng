899c18bd5f98d5a0c1aebe9e4700584e3c365e34
watcharapat <plengslowtoad>
2026-02-18 13:56:10 +0700

    2.Handleallactionsincomponentsslice(store/index.js

--- src/store/components.js
+++ src/store/components.js
@@ -10,12 +10,29 @@ export const componentsSlice = createSlice({
   initialState,
   reducers: {
     addComponent: (state, action) => {
+      const newComponent = {
+        id: action.payload.id,
+        layout: action.payload.layout,
+        values: {},
+      };
+      state.items.push(newComponent);
+      state.currentlyEdited = newComponent;
     },
     updateComponent: (state, action) => {
+      const { id, data } = action.payload;
+      const item = state.items.find(item => item.id === id);
+      if (item) {
+        item.values = data.values;
+      }
+      state.currentlyEdited = null;
     },
     removeComponent: (state, action) => {
+      const { id } = action.payload;
+      state.items = state.items.filter(item => item.id !== id);
+      state.currentlyEdited = null;
     },
     setEditedComponent: (state, action) => {
+      state.currentlyEdited = action.payload.component;
     },
   },
 })
